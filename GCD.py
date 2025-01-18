65616b31f38ee40f693bd4fe3f6f2b90bcb8b618
Rudranil Das <rudranild04@gmail.com>
2025-01-18 20:55:36 +0530

    Create GCD.py

---
+++ GCD.py
@@ -0,0 +1,9 @@
+def gcd(num1, num2):
+  if (b==0):
+    return a
+  else:
+      return gcd(b, a%b)
+
+num1 = int(input("Enter no.: "))
+num2 = int(input("Enter no.: "))
+print(f"GCD of {num1} and {num2} is : ",gcd(num1, num2)
