45383e1bf77bdc00ee1c466fbb1989d3fc340d6c
Rudranil Das <rudranild04@gmail.com>
2025-01-20 19:54:08 +0530

    Update and rename rock-paper-scissors_file.py to rps-game_file.py

--- rps-game_file.py
+++ rps-game_file.py
@@ -3,11 +3,10 @@ choices=["rock", "paper", "scissors"]
 while True:
   with open("rock_paper_scissors.txt", "w") as file:
     user=input("Enter your choice: ").lower()
-    comp=random.choice(choices)
-    
-    if(user==computer):
-      result = "It's a tie."
-    elif(user=="paper" and comp=="rock" or user =="rock" and comp=="scissors" or user=="scissors" and comp=="paper"):
+
+    rem_choice= [choice for choice in choices if choice != user]
+    comp=random.choice(rem_choices)
+    if(user=="paper" and comp=="rock" or user =="rock" and comp=="scissors" or user=="scissors" and comp=="paper"):
       result = "User wins!"
     else:
       result = "Computer wins!"
